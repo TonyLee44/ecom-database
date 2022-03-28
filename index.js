66269af5c3e340cde6bb80501291ee9a06df9a78
Tony Lee <tonylee@R2Hs-MBP.lan>
2022-03-28 19:05:22 -0400

    pushing database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql');
+const path = require('path')
 
 const app = express();
 
@@ -21,7 +22,11 @@ db.connect(err => {
 
 app.use(cors());
 app.use(express.json());
-
+app
+.use(express.static(path.join(__dirname, 'public')))
+.set('views', path.join(__dirname, 'views'))
+.set('view engine', 'ejs')
+.get('/', (req, res) => res.render('pages/index'))
 app.get("/api/products", (req, res) => {
     db.query(`SELECT * FROM products`, (err, result) => {
         if (err) {
