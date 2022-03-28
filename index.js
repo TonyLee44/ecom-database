5d73bee9a3ae8e0b205ecd1ce97e11a005302284
Tony Lee <tonylee@R2Hs-MBP.lan>
2022-03-28 19:35:46 -0400

    update

--- index.js
+++ index.js
@@ -22,11 +22,10 @@ db.connect(err => {
 
 app.use(cors());
 app.use(express.json());
-app
-.use(express.static(path.join(__dirname, 'public')))
-.set('views', path.join(__dirname, 'views'))
-.set('view engine', 'ejs')
-.get('/', (req, res) => res.render('pages/index'))
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
 app.get("/api/products", (req, res) => {
     db.query(`SELECT * FROM products`, (err, result) => {
         if (err) {
